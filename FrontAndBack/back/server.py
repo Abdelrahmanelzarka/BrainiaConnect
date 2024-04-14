3abd2f96f222e9af3235a5ed04a2cef936a24de6
Abdelrahmanelzarka <abdelrahamnelzarka18@gmail.com>
2024-04-14 15:33:57 +0200

    try the model

--- FrontAndBack/back/server.py
+++ FrontAndBack/back/server.py
@@ -1,12 +1,56 @@
 from flask import Flask,jsonify, make_response
 import random
 import string
+import tensorflow as tf
+import socket
 
 app = Flask(__name__)
 
 
-@app.route("/",methods=["GET"])
+
+# Define host (usually empty string for listening on all interfaces) and port
+HOST = ''
+PORT = 1000
+data = b''
+
+# Create a TCP socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+
+        # Initialize an empty variable to store received data
+        
+
+        # Continuously receive data in a loop (replace 1024 with desired buffer size)
+        while True:
+            recieved_data = conn.recv(1024)
+            if not data:  # Check if connection closed
+                break
+            data += recieved_data  # Append received data to the variable
+
+
+def load_model():
+    global model  # Declare `model` as a global variable
+    model = tf.keras.models.load_model('main_net_3.h5 copy') 
+
+load_model()
+
+
+@app.route('/predict', methods=['GET'])
 def predict():
+    prediction = model.predict(data)
+    data= b''
+
+    return jsonify({'prediction': prediction.tolist()})
+  
+
+
+
+@app.route("/",methods=["GET"])
+def get():
   """Returns a random lowercase alphabet character."""
   alphabet = string.ascii_lowercase  # Get lowercase alphabet string
   alphabet = list(alphabet)
