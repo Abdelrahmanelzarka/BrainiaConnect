cb7727351c49ee3c8106107f991029e6aa7dc0e9
Kareem Akram <61750924+xXKareemXx@users.noreply.github.com>
2024-05-05 03:13:57 +0300

    Multithreading.py

--- EEG_Python/Multithreading.py
+++ EEG_Python/Multithreading.py
@@ -1,10 +1,7 @@
 import threading
-import queue  # for communication between threads
 import GUI, main2
 
-# Main scripts
 def run_eeg_recording():
-    # Replace with the actual call to your EEG recording script
     EEG_recording.main()
 
 def run_gui():
@@ -12,13 +9,11 @@ def run_gui():
 
 
 if __name__ == '__main__':
-    # Create and start threads
     eeg_thread = threading.Thread(target=run_eeg_recording)
     gui_thread = threading.Thread(target=run_gui)
 
     eeg_thread.start()
     gui_thread.start()
 
-    # Wait for both threads to finish (optional)
     eeg_thread.join()
     gui_thread.join()
